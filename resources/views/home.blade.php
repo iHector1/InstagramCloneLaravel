5061be8a0d97e245392e90fadbaa8311523e44c3
H <62352013+iHector1@users.noreply.github.com>
2024-01-02 21:41:40 -0600

    creacion de usuario con nombre de usuario

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+    <div class="col-3 p-5">
+    <img src="https://i.pinimg.com/originals/84/90/2c/84902cdd8579de857eab0b29e95f753a.png" class="rounded-circle w-50" alt="">
+        
+    </div>
+    <div class="col-9 pt-5">
+            <div><h1>freecode</h1></div>
+            <div class="d-flex">
+                <div class="px-5"><strong>153</strong> posts</div>
+                <div class="px-5"><strong>233</strong> followers</div>
+                <div class="px-5"><strong>233</strong> following</div>
+            </div>
+            <div class="pt-4 fw-bold">
+                freecode
+            </div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sequi quaerat assumenda quo, ipsum, adipisci, sint et repellat ad necessitatibus dolorum nesciunt quia praesentium blanditiis inventore debitis. Commodi, nemo?</div>
+            <div><a href="#">hola.com</a></div>
+
+            
+    </div> 
+    <div class="row pt-5">
+                <div class="col-4">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA0NyuP6zMt6Ctta2VYpNlY5qYItDgfIfaBdS_xo5WLw&s" class="w-100">
+                </div>
+                <div class="col-4">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA0NyuP6zMt6Ctta2VYpNlY5qYItDgfIfaBdS_xo5WLw&s" class="w-100">
+                </div>
+                <div class="col-4">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA0NyuP6zMt6Ctta2VYpNlY5qYItDgfIfaBdS_xo5WLw&s" class="w-100">
+                </div>
+            </div>
+   </div>
+</div>
+@endsection
